6c950becc1f295247e3c43e79f084c9c5363056b
PAN001 <pan.atopos@gmail.com>
2019-04-19 18:08:46 -0400

    test

--- src/openalpr/binarize_wolf.cpp
+++ src/openalpr/binarize_wolf.cpp
@@ -110,11 +110,11 @@ namespace alpr
     // The threshold T for the center pixel of the window is computed using the mean m and the variance s of the gray values in the window:
     // T = m + k · s, where k is a constant set to −0.2.
     void NiblackSauvolaWolfJolion (Mat im, Mat im_sum, Mat im_sum_sq, double min_I, double max_I, Mat output, NiblackVersion version,
+        int winx, int winy, double k, double dR) {
+
         std::cout << "========================== NiblackSauvolaWolfJolion ==========================" << std::endl;
         timespec startTime;
         getTimeMonotonic(&startTime);
-
-        int winx, int winy, double k, double dR) {
 
         double m, s, max_s;
         double th=0;
