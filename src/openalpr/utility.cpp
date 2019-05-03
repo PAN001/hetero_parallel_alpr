4fd947e4ddac69e4a7a8d863978072fa21679326
PAN001 <pan.atopos@gmail.com>
2019-05-03 10:09:38 -0400

    test

--- src/openalpr/utility.cpp
+++ src/openalpr/utility.cpp
@@ -176,14 +176,14 @@ namespace alpr
 
 
     for (int i = 0; i < THRESHOLD_COUNT; i++) {
-      // cout << "Showing threshold: " << i << endl;
-      // std::string outputname;
-      // std::stringstream ss;
-      // ss << i << "_threshold.jpg";
-      // outputname = ss.str();
+      cout << "Showing threshold: " << i << endl;
+      std::string outputname;
+      std::stringstream ss;
+      ss << i << "_threshold.jpg";
+      outputname = ss.str();
       // imwrite(outputname, thresholds[i]);
 
-      // displayImage(config, "Binarization  Thresholds", thresholds[i]);
+      displayImage(config, "Binarization  Thresholds", thresholds[i]);
     }
 
     return thresholds;
